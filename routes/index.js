c1d118ea325175f6af35fafd1c487c275d3c1762
unknown <bazanjuanjordan@gmail.com>
2023-09-30 01:17:10 -0300

    Se cra Controladores de servicios

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const userRoute = require('./user')
+const servicioRoute = require('./servicio')
 
 router.use('/user',userRoute)
+router.use('/servicio',servicioRoute)
 module.exports = router
