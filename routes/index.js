15870302754f9b40ea5db71ed0f7370ad5c7d851
unknown <bazanjuanjordan@gmail.com>
2023-10-07 17:07:01 -0300

    se crea rama cloudinary

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const habitacionRouter = require('./habitacion')
 const cartServicioRouter = require('./cartServicio')
 const reservaRoute = require('./reserva')
 const messageRoute = require('./message')
+const imageRoute = require('./image')
 
 
 router.use('/user',userRoute)
@@ -14,6 +15,7 @@ router.use('/habitacion',habitacionRouter)
 router.use('/cartServicio',cartServicioRouter)
 router.use('/reserva',reservaRoute)
 router.use('/message',messageRoute)
+router.use('/image',imageRoute)
 
 
 module.exports = router
